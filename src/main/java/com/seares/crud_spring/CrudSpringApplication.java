66e4450fd470bd84b72e6f339304088db6df8395
Caio Torres Seares <caiot.seares@gmail.com>
2025-02-18 10:51:56 -0300

    feat: add GET /api/courses endpoint to list all courses

--- src/main/java/com/seares/crud_spring/CrudSpringApplication.java
+++ src/main/java/com/seares/crud_spring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.seares.crud_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.seares.crud_spring.model.Course;
+import com.seares.crud_spring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,15 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("Front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
---
+++ src/main/java/com/seares/crud_spring/controller/CourseController.java
@@ -0,0 +1,29 @@
+package com.seares.crud_spring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.seares.crud_spring.model.Course;
+import com.seares.crud_spring.repository.CourseRepository;
+
+@RestController
+@RequestMapping("/api/courses")
+
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
