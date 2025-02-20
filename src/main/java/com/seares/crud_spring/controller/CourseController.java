e580d0cfc2f39504e12edef6fef2745c776d05ea
Caio Torres Seares <caiot.seares@gmail.com>
2025-02-20 07:47:57 -0300

    feat: add course creation endpoint

--- src/main/java/com/seares/crud_spring/controller/CourseController.java
+++ src/main/java/com/seares/crud_spring/controller/CourseController.java
@@ -2,7 +2,11 @@ package com.seares.crud_spring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +30,13 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    //@ResponseStatus(code = HttpStatus.CREATED) Caso utilizar o Response status, dá para remover o ResponseEntity, e retornar apenas o que foi criado.
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        //return courseRepository.save(course);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(courseRepository.save(course));
     }
 }
