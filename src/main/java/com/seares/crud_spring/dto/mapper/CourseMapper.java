313f466c27f28f0d688d6c0085bd7195baa2153d
Caio Torres Seares <caiot.seares@gmail.com>
2025-03-03 13:01:40 -0300

    refactor: update code to use LessonDTO instead of Lesson

--- src/main/java/com/seares/crud_spring/dto/mapper/CourseMapper.java
+++ src/main/java/com/seares/crud_spring/dto/mapper/CourseMapper.java
@@ -1,19 +1,27 @@
 package com.seares.crud_spring.dto.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.seares.crud_spring.dto.CourseDTO;
+import com.seares.crud_spring.dto.LessonDTO;
 import com.seares.crud_spring.enums.Category;
 import com.seares.crud_spring.model.Course;
 
 @Component
 public class CourseMapper {
     public CourseDTO toDTO(Course course) {
-        if (course == null){
+        if (course == null) {
             return null;
         }
+        List<LessonDTO> lessons = course.getLessons().stream()
+                .map(lesson -> new LessonDTO(lesson.getId(), lesson.getName(), lesson.getYoutubeUrl()))
+                .collect(Collectors.toList());
+
         return new CourseDTO(course.getId(), course.getName(), course.getCategory().getValue(),
-                course.getLessons());
+                lessons);
     }
 
     public Course toEntity(CourseDTO courseDTO) {
@@ -21,7 +29,7 @@ public class CourseMapper {
         if (courseDTO == null) {
             return null;
         }
-        
+
         Course course = new Course();
         if (courseDTO.id() != null) {
             course.setId(courseDTO.id());
@@ -37,7 +45,7 @@ public class CourseMapper {
         }
         return switch (value) {
             case "Back-end" -> Category.BACK_END;
-            case "Front-end"-> Category.FRONT_END;
+            case "Front-end" -> Category.FRONT_END;
             default -> throw new IllegalArgumentException("Invalid value: " + value);
         };
     }
