633ff1ccb799515eafffa071a11d10f3b4c77545
Caio Torres Seares <caiot.seares@gmail.com>
2025-03-04 10:50:47 -0300

    feat: validations error

--- src/main/java/com/seares/crud_spring/dto/LessonDTO.java
+++ src/main/java/com/seares/crud_spring/dto/LessonDTO.java
@@ -1,8 +1,13 @@
 package com.seares.crud_spring.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record LessonDTO(
         Long id,
-        String name,
-        String youtubeUrl) {
+        @NotNull @NotBlank @Length(min = 5, max = 100) String name,
+        @NotNull @NotBlank @Length(min = 5, max = 100) String youtubeUrl) {
 
 }
